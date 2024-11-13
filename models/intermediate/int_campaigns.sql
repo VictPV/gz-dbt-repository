36f8de4a7b3336d9b9fc0372f1a45697e6d102d9
Victoria Petrova <victoria.petrova286@gmail.com>
2024-11-13 16:24:38 +0000

    use of macro function dbt_utils.union_relations from package dbt_utils which replace UNION ALL in int_campaigns.sql

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,12 +1,9 @@
-SELECT *
-FROM {{ ref('stg_raw__adwords') }}
-UNION ALL
-SELECT *
-FROM {{ ref('stg_raw__bing') }}
-UNION ALL
-SELECT *
-FROM {{ ref('stg_raw__criteo') }}
-UNION ALL
-SELECT *
-FROM {{ ref('stg_raw__facebook') }}
-SELECT
+
+-- int_campaigns.sql
+ {{
+     dbt_utils.union_relations(
+         relations=[ref('stg_raw__adwords'), ref('stg_raw__bing'), ref('stg_raw__criteo'), ref('stg_raw__facebook')]
+     )
+ }}
+
+ 
