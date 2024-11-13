6aa4ff81671238fa951ca03be5584fb95462df87
Victoria Petrova <victoria.petrova286@gmail.com>
2024-11-13 16:02:24 +0000

    create a margin percent Function

---
+++ macros/functions.sql
@@ -0,0 +1,6 @@
+
+-- functions.sql
+ {% macro margin_percent(revenue, purchase_cost, precision=2) %}
+     ROUND( SAFE_DIVIDE( ({{ revenue }} - {{ purchase_cost }}) , {{ revenue }} ) , {{ precision }})
+ {% endmacro %}
+
