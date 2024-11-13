3761ff1051988db9cd38797c617da00a0c78a09c
Victoria Petrova <victoria.petrova286@gmail.com>
2024-11-13 09:09:13 +0000

    changement de code sur int_orders_margin et int_sales_margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,6 +1,6 @@
 SELECT
-DISTINCT (orders_id) AS orders_id,
-date_date AS date_date,
+orders_id,
+date_date,
 ROUND(SUM (revenue),2) AS turnover,
 ROUND(SUM(quantity),2) AS quantity,
 ROUND(SUM(purchase_cost),2) AS purchase_cost,
@@ -8,3 +8,4 @@ ROUND(SUM(margin),2) AS margin
 FROM {{ ref('int_sales_margin') }}
 GROUP BY 
     orders_id, date_date
+ORDER BY orders_id DESC
